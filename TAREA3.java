8f0bc0de7458768f2c3eec2fd52874931b839b72
Alexbarcenas7 <barcenasdavalosalejandro2020@gmail.com>
2024-09-24 00:01:34 -0600

    Actualizar TAREA11.java

--- TAREA3.java
+++ TAREA3.java
@@ -1,48 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        String palabraTarea = "supercalifragilisticoespialidoso";
 
-/*
-informacion: libro favorito
- */
+        int longitud = palabraTarea.length();
+        String tresDoce = palabraTarea.substring(5, 15);
+        int primeraOcurrenciaI = palabraTarea.lastIndexOf('i');
+        int ultimaOcurrenciaO = palabraTarea.lastIndexOf('o');
+        System.out.println("Última ocurrencia de 'o': " + palabra.lastIndexOf('o'));
 
-        System.out.println("el titulo del libro es Siddhartha");
-        System.out.println("El autor es Hermann Hesse");
-        System.out.println("Su año de publicacion fue en 1922");
-        System.out.println("Su genero literario es Novela filosofica");
-        System.out.println("tiene 152 paginas");
+        System.out.println("En minúsculas: " + palabra.toLowerCase());
+        
+        System.out.println("Subcadena desde la posición 7: " + palabra.substring(6));
 
-        /*
-informacion: pais que deseo visitar
- */
-
-        String nombrePais = "Japon";
-        String capital = "tokio";
-        String idiomaOficial = "japones";
-        Double  poblacionEnMillones = 125.1;
-        String moneda = "el yen";
-
-        System.out.println(" El pais es: " + nombrePais);
-        System.out.println(" La capital es: " + capital);
-        System.out.println(" El idioma es: " + idiomaOficial);
-        System.out.println(" La poblacion es: " + poblacionEnMillones);
-        System.out.println(" Su moneda es: " + moneda);
-
-        /*
- informacion: personaje historico favorito
- */
-
-        String nombrePersonaje = "Tariacuri";
-        int añoNacimiento = 1320;
-        String nacionalidad = "Mexicana?";
-        String ocupacion = "rey del imperio purepecha";
-        String logroDestacado = "fundacion del imperio purepecha";
-
-        System.out.println(" Su nombre fue: " + nombrePersonaje);
-        System.out.println(" Nacio en el año de: " + añoNacimiento);
-        System.out.println(" Su nacionalidad es de: " + nacionalidad);
-        System.out.println(" Su ocupacion fue la de: " + ocupacion );
-        System.out.println(" Su mayor logro fue: " + logroDestacado);
+        System.out.println("Última ocurrencia de 'li': " + palabra.lastIndexOf("li"));
 
     }
 }
