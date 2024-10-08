057255edf3057556e242cb8753ad4a9cd7ca3e56
Alejandro :) <barcenasdavalosalejandro2020@gmail.com>
2024-10-07 22:49:21 -0600

    Create tarea3.java

---
+++ tarea3.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+/*
+informacion: libro favorito
+ */
+
+        System.out.println("el titulo del libro es Siddhartha");
+        System.out.println("El autor es Hermann Hesse");
+        System.out.println("Su año de publicacion fue en 1922");
+        System.out.println("Su genero literario es Novela filosofica");
+        System.out.println("tiene 152 paginas");
+
+        /*
+informacion: pais que deseo visitar
+ */
+
+        String nombrePais = "Japon";
+        String capital = "tokio";
+        String idiomaOficial = "japones";
+        Double  poblacionEnMillones = 125.1;
+        String moneda = "el yen";
+
+        System.out.println(" El pais es: " + nombrePais);
+        System.out.println(" La capital es: " + capital);
+        System.out.println(" El idioma es: " + idiomaOficial);
+        System.out.println(" La poblacion es: " + poblacionEnMillones);
+        System.out.println(" Su moneda es: " + moneda);
+
+        /*
+ informacion: personaje historico favorito
+ */
+
+        String nombrePersonaje = "Tariacuri";
+        int añoNacimiento = 1320;
+        String nacionalidad = "Mexicana?";
+        String ocupacion = "rey del imperio purepecha";
+        String logroDestacado = "fundacion del imperio purepecha";
+
+        System.out.println(" Su nombre fue: " + nombrePersonaje);
+        System.out.println(" Nacio en el año de: " + añoNacimiento);
+        System.out.println(" Su nacionalidad es de: " + nacionalidad);
+        System.out.println(" Su ocupacion fue la de: " + ocupacion );
+        System.out.println(" Su mayor logro fue: " + logroDestacado);
+
+    }
+}
