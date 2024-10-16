cc52d43b78809e99f3af9c07390de934f911b9ad
Alejandro :) <barcenasdavalosalejandro2020@gmail.com>
2024-10-15 23:03:56 -0600

    tarea3unidad3.java

---
+++ tarea3unidad3.java
@@ -0,0 +1,78 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+                // Operación 1: 3 < 5 && 7 >= 2
+                if (3 < 5 && 7 >= 2) {
+                    System.out.println("El resultado de la operación 3 < 5 && 7 >= 2 es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación 3 < 5 && 7 >= 2 es falso");
+                }
+
+                // Operación 2: 10 != 8 || 15 == 15
+                if (10 != 8 || 15 == 15) {
+                    System.out.println("El resultado de la operación 10 != 8 || 15 == 15 es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación 10 != 8 || 15 == 15 es falso");
+                }
+
+                // Operación 3: !(5 > 6) && 2 <= 3
+                if (!(5 > 6) && 2 <= 3) {
+                    System.out.println("El resultado de la operación !(5 > 6) && 2 <= 3 es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación !(5 > 6) && 2 <= 3 es falso");
+                }
+
+                // Operación 4:
+                if ((8 == 8 || 113 < 1) && 6 > 2) {
+                    System.out.println("El resultado de la operación (8 == 8 || 113 < 1) && 6 > 2 es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación (8 == 8 || 113 < 1) && 6 > 2 es falso");
+                }
+
+                // Operación 5: 12 != 10 && (4 < 8 || 9 == 9)
+                if (12 != 10 && (4 < 8 || 9 == 9)) {
+                    System.out.println("El resultado de la operación 12 != 10 && (4 < 8 || 9 == 9) es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación 12 != 10 && (4 < 8 || 9 == 9) es falso");
+                }
+
+                // Operación 6: (3 + 5 == 8) && (7 * 2 <= 14)
+                if ((3 + 5 == 8) && (7 * 2 <= 14)) {
+                    System.out.println("El resultado de la operación (3 + 5 == 8) && (7 * 2 <= 14) es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación (3 + 5 == 8) && (7 * 2 <= 14) es falso");
+                }
+
+                // Operación 7: !(9 <= 5) && (4 == 4 || 6 != 6)
+                if (!(9 <= 5) && (4 == 4 || 6 != 6)) {
+                    System.out.println("El resultado de la operación !(9 <= 5) && (4 == 4 || 6 != 6) es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación !(9 <= 5) && (4 == 4 || 6 != 6) es falso");
+                }
+
+                // Operación 8: 10 % 2 == 0 && (5 > 1 || 12 != 10)
+                if (10 % 2 == 0 && (5 > 1 || 12 != 10)) {
+                    System.out.println("El resultado de la operación 10 % 2 == 0 && (5 > 1 || 12 != 10) es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación 10 % 2 == 0 && (5 > 1 || 12 != 10) es falso");
+                }
+
+                // Operación 9: (3 * 3 == 9) && !(8 < 5 || 7 >= 9)
+                if ((3 * 3 == 9) && !(8 < 5 || 7 >= 9)) {
+                    System.out.println("El resultado de la operación (3 * 3 == 9) && !(8 < 5 || 7 >= 9) es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación (3 * 3 == 9) && !(8 < 5 || 7 >= 9) es falso");
+                }
+
+                // Operación 10: !(5 + 5 != 10) && (2 * 3 > 5 || 6 == 7)
+                if (!(5 + 5 != 10) && (2 * 3 > 5 || 6 == 7)) {
+                    System.out.println("El resultado de la operación !(5 + 5 != 10) && (2 * 3 > 5 || 6 == 7) es verdadero");
+                } else {
+                    System.out.println("El resultado de la operación !(5 + 5 != 10) && (2 * 3 > 5 || 6 == 7) es falso");
+                }
+
+
+            }
+        }
+
